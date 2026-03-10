4d7564a7ffd123c811e4d6412c746671ba4cd3b3
Hemanth-KumarReddy <hemanthreddy10925@gmail.com>
2026-03-10 21:30:04 +0530

    Hotel and Restaurant Booking Project

---
+++ src/main/java/com/HotelAndRestuarantBooking/HotelAndRestuarantBookingApplication.java
@@ -0,0 +1,13 @@
+package com.HotelAndRestuarantBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelAndRestuarantBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelAndRestuarantBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HotelAndRestuarantBooking/HotelAndRestuarantBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HotelAndRestuarantBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelAndRestuarantBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
